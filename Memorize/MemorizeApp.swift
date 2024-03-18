99cdbe2f864844b280b459d5c3053b5f8d077aaf
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-03-18 12:13:51 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Griffin Baker on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
