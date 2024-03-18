cafb45ba6b9675d1dd8c72b5972813cd7d3c3dfa
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-03-18 13:18:10 -0500

    init and some card design

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+// declaring a type here named ContentView, and it is a struct
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            // this was just like the default that we passed the Image, we just overwrote the default
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                // this percolates from above
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
     }
 }
 
