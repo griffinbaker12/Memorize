d665955503b2b2b61453923b6a1b73903ea10e09
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-03-20 14:19:12 -0500

    grid, refactor into vars, adding scroll view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,17 +9,58 @@ import SwiftUI
 
 // declaring a type here named ContentView, and it is a struct
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🕷️",  "😈"]
-
+    let emojis = ["👻", "🎃", "🕷️", "😈", "🧛", "👽", "🤖", "🧟", "💀", "👹", "🕹️", "🪩"];
+    
+    @State var cardCount = 4
+    
     // putting View here makes no sense, because we are supposed to be telling the compiler what View we actually want to return here
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.small)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        let countPlusOffset = cardCount + offset
+        return Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(countPlusOffset < 1 || countPlusOffset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
@@ -31,20 +72,18 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                // this percolates from above
-                // don't need the .fill modifier, but that is what is happening above
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             // self, or the view itself in this case, is immutable...and even thought that is a var, that "varness" is only at the very beginning (without the @State)
             // @State is for small things, like flipping the card over, not the state of the game or anything like that
-//            isFaceUp = !isFaceUp
+            //            isFaceUp = !isFaceUp
             isFaceUp.toggle()
         }
     }
